f6bc2f26d45cb34ab9bcfd080f767274b5cebda8
Doug DeJong <dougdejong@gmail.com>
2018-10-03 17:38:26 -0400

    added some basic components

---
+++ src/components/PostList/PostList.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+// import AddProject from './AddProject';
+
+class PostList extends Component {
+  constructor(){
+      super();
+      this.state = { listOfProjects: [] };
+  }
+
+  getAllPosts = () =>{
+    axios.get(`http://localhost:5000/api/posts`)
+    .then(responseFromApi => {
+      this.setState({
+        listOfPosts: responseFromApi.data
+      })
+    })
+  }
+
+  componentDidMount() {
+    this.getAllPosts();
+  }
+
+  render(){
+    return(
+      <div>
+        <div style={{width: '60%', float:"left"}}>
+          { this.state.listOfPosts.map((post, index) => {
+            return (
+              <div key={post._id}>
+                <Link to={`/posts/${post._id}`}>
+                  <h3>{post.title}</h3>
+                </Link>
+                <p style={{maxWidth: '400px'}} >{post.content} </p>
+              </div>
+            )})
+          }
+        </div>
+        <div style={{width: '40%', float:"right"}}>
+            {/* <AddProject getData={() => this.getAllProjects()}/> */}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default ProjectList;
