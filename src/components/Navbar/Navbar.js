4df30a244a0215b39c3c16ce5974f1995821518b
Doug DeJong <dougdejong@gmail.com>
2018-10-08 11:45:42 -0400

    added log in and log out routes

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,16 +1,54 @@
-// components/navbar/Navbar.js
-
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import AuthService from '../Auth/auth-service';
 
-const navbar = () => {
-  return (
-    <nav className="nav-style">
-      <ul>
-        <li><Link to="/posts" style={{ textDecoration: 'none' }}>Posts</Link></li>
-    </ul>
-    </nav>
-  )
-}
 
-export default navbar;
+class Navbar extends Component {
+  constructor(props){
+    super(props);
+    this.state = { loggedInUser: null };
+    this.service = new AuthService();
+
+  }
+  
+  componentWillReceiveProps(nextProps) {
+    this.setState({ loggedInUser: nextProps["userInSession"]})
+  }
+
+
+  logout = () =>{
+    this.service.logout()
+    .then(()=>{
+      this.props.setTheUserInTheAppComponent(null)
+    })
+  }
+    
+  render(){
+    if(this.state.loggedInUser){
+      return(
+        <nav className="navbar">
+          <ul>
+            <li>Welcome, {this.state.loggedInUser.username}</li>
+            <li>
+            <Link to="/posts" style={{ textDecoration: 'none' }}>Posts</Link>
+            </li>
+            <li>
+              <button onClick={()=>this.logout()}>Log Out</button>
+            </li>
+          </ul>
+        </nav>
+      )
+    } else {
+      return (
+        <div>
+        <nav className="navbar">
+          <ul>
+            <li><Link to='/signup' style={{ textDecoration: 'none' }}>Signup</Link></li>
+          </ul>
+        </nav>
+        </div>
+      )
+    }
+    }
+  }
+  export default Navbar;
