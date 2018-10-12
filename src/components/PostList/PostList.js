790c80971ea415bcdf8dba0961a3ba5f396ab3a6
Doug DeJong <dougdejong@gmail.com>
2018-10-12 12:23:03 -0400

    finished styling

--- src/components/PostList/PostList.js
+++ src/components/PostList/PostList.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from 'react-html-parser';
-
+import PostIcon from './images/doug-icon.png';
 // import AddProject from './AddProject';
 
 class PostList extends Component {
@@ -27,18 +27,26 @@ class PostList extends Component {
   render(){
     return(
       <div>
-        <div className="postList" >
+        <div className="postList container-fluid" >
           { this.state.listOfPosts.map((post, index) => {
             return (
+              <div className="row">
               <div className='postCard col-6' key={post._id}>
               <div className="row">
-              <div className='postInfo col-4'>
-                <Link to={`/posts/${post._id}` } style={{ textDecoration: 'none', color: 'white', float: "left"}}>
+              <div className='postInfo col-6'>
+                <Link to={`/posts/${post._id}` } style={{ textDecoration: 'none', color: 'white', }}>
                   <h3 className="postTitle">{post.title}</h3>
                 </Link>
+  
                 <h5 className="postUser"> by {post.author.username} </h5>
                </div>
-                <p className="postBlerb col-4" >{post.blerb} </p>
+               <div className="col-4">
+                <p className="postBlerb" > {post.blerb} </p>
+                </div>
+                <div className="col-2">
+                <img className="postIcon" alt="postIcon" src={PostIcon}/>
+              </div>
+              </div>
               </div>
               </div>
             )})
